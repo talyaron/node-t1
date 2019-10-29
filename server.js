ed684e7bc8aa0be97ba65cfb15eae47778c8eb43
Tal Yaron <tal.yaron@gmail.com>
2019-10-29 08:59:28 +0200

    switched

--- server.js
+++ server.js
@@ -1,22 +1,63 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-
+const express = require("express");
 const app = express();
-const people = ['Ben', 'Josef', 'Rebeka']
+const port = 3001;
+const bodyParser = require("body-parser");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const yargs = require("yargs");
 
-app.use(express.static('public'))
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home', { test: 'testing', publish: false, people: people })
-})
 
-let port = process.env.PORT || 3000;
-app.set('port', port);
-app.listen(app.get('port'), function () {
-    console.log('Server liten on port', port)
-})
+const arguments = yargs.argv;
+
+if (arguments.init) {
+  const MongoClient = require("mongodb").MongoClient;
+  const url = "mongodb://localhost:27017/sampleDB";
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    const dbo = db.db("sampleDB");
+    const students = [
+      { name: "Micheal", last:"Boorg", average: 78 },
+      { name: "Motti", last:"Hill", average: 90 },
+      { name: "Hana",last:"Mount", average: 92 }
+    ];
+    dbo
+      .collection("students")
+      .insertMany(students)
+      .then(res => {
+        if (err) throw err;
+        console.log(res);
+        console.log("1 document inserted");
+        db.close();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+}
+
+
+
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+const publicPath = path.join(__dirname, './views');
+app.use('/', express.static(publicPath));
+
+
+const routes = require("./routes/index");
+app.use("/", routes);
+
+
+
+app.listen(port, () => console.log(`server listening on port ${port}!`));
